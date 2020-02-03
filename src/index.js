b3f4cf5e2a1ed4c255e2f2e43abf696effe1a913
morozov1982i <60391836+morozov1982i@users.noreply.github.com>
2020-02-03 16:37:42 +0600

    Завершил подготовку проекта

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-fetch("https://swapi.co/api/people/1/")
-  .then(res => {
-    return res.json();
-  })
-  .then(body => {
-    console.log(body);
-  });
+class SwapiService {
+  _apibase = "https://swapi.co/api";
+
+  async getResouce(url) {
+    const res = await fetch(`${this._apibase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResouce(`/people/`);
+    return res.results;
+  }
+  getPerson(id) {
+    return this.getResouce(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResouce(`/planets/`);
+    return res.results;
+  }
+  getPlanet(id) {
+    return this.getResouce(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResouce(`/starships/`);
+    return res.results;
+  }
+  getStarship(id) {
+    return this.getResouce(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllStarships().then(people => {
+  console.log(people);
+});
